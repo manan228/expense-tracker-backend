e8907bfa22b7383975b9baa63e29c00ac25770ea
Manan <manan228@yahoo.in>
2022-10-13 18:44:43 +0530

    rmoved console in app.js

--- app.js
+++ app.js
@@ -18,8 +18,6 @@ const User = require("./models/user");
 const Expense = require("./models/expense");
 const FileDownloaded = require("./models/fileDownloaded");
 
-console.log(`testing cicd pipleine`);
-
 const accessLogStream = fs.createWriteStream(
   path.join(__dirname, "access.log"),
   { flag: "a" }
